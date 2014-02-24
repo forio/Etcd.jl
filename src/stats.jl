615cf1d60f19baaa42bfe932e256e0eb0726543b
Bassem Youssef <bassem@outlook.com>
2014-02-24 08:44:52 -0800

    stats

--- src/stats.jl
+++ src/stats.jl
@@ -5,9 +5,9 @@ stats_prefix(etcd::EtcdServer,
              stats_type) = "http://$(etcd.ip):$(etcd.port)/v2/stats/$(stats_type)"
 
 function stats(etcd::EtcdServer,stats_type::String)
-    if is(stats_type,"leader") ||
-       is(stats_type,"self") ||
-       is(stats_type,"store")
+    if stats_type != "leader" &&
+       stats_type != "self" &&
+       stats_type != "store"
         warn("Stats : Must specify the stats_type to be one of: leader, self or store")
     else
         etcd_request(Requests.get,stats_prefix(etcd,stats_type)) |>
